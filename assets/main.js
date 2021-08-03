2c27b1ab36899f92c3f2e7a9945a34d4894674cf
Sweet Acid <antoniu.ilea@gmail.com>
2021-08-03 05:42:58 +0300

    Minimal ES6 features so the code runs....

--- assets/main.js
+++ assets/main.js
@@ -12,31 +12,29 @@ function search(query) {
     let url = new URL("https://g.tenor.com/v1/search");
     url.search = params.toString();
 
-    return fetch(url)
-        .then(res => res.json())
-        .then(results => {
-            return {
-                gifs: results.results,
-                done: results.next === "0",
-            };
-        })
-        .catch(error => {
-            console.error(error);
+    return new Promise(function (resolve, reject) {
+        httpGetAsync(url.toString(), function (results) {
+            let response_objects = JSON.parse(results);
 
-            return error;
+            return resolve({
+                gifs: response_objects.results,
+                done: response_objects.next === "0",
+            });
         });
+    });
 }
+
 let queryElem = document.getElementById("query");
 let searchElem = document.getElementById("search");
 let containerElem = document.getElementById("container");
 
-searchElem.onclick = () => {
+searchElem.onclick = function () {
     while (containerElem.children.length)
         containerElem.removeChild(containerElem.firstChild);
 
     search(queryElem.value)
-        .then(({ gifs, done }) => {
-            gifs.map(gif => {
+        .then(function (result) {
+            result.gifs.map(gif => {
                 return gif.media[0].nanomp4.url;
             }).forEach(url => {
                 containerElem.appendChild(createVideoElem(url));
@@ -45,10 +43,10 @@ searchElem.onclick = () => {
             function createVideoElem(src) {
                 let video = document.createElement("video");
                 video.src = src;
-                video.onmouseenter = () => {
+                video.onmouseenter = function () {
                     video.play();
                 };
-                video.onmouseleave = () => {
+                video.onmouseleave = function () {
                     video.pause();
                     video.currentTime = 0;
                 };
@@ -57,3 +55,24 @@ searchElem.onclick = () => {
             }
         });
 };
+
+// url Async requesting function
+function httpGetAsync(theUrl, callback) {
+    // create the request object
+    var xmlHttp = new XMLHttpRequest();
+
+    // set the state change callback to capture when the response comes in
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
+    }
+
+    // open as a GET call, pass in the url and set async = True
+    xmlHttp.open("GET", theUrl, true);
+
+    // call send with no params as they were passed in on the url string
+    xmlHttp.send(null);
+
+    return;
+}
